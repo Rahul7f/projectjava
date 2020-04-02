65fb692c6f979ffabdc4e38b3eced1259a994213
Rahul <rahu.singhl54321@gmail.com>
2020-04-02 13:17:57 +0530

    test

--- app/src/main/java/com/example/javatutorial/javalist.java
+++ app/src/main/java/com/example/javatutorial/javalist.java
@@ -5,19 +5,45 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class javalist extends AppCompatActivity {
     private androidx.appcompat.widget.Toolbar toolbar;
+
+    ListView listView;
+    TextView textView;
+    String[] listItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_javalist);
         toolbar = findViewById(R.id.main_page_toolbar);
-        setSupportActionBar(toolbar);
+        listView = findViewById(R.id.showlist);
+        textView = findViewById(R.id.txtview);
+        listItem = getResources().getStringArray(R.array.Basic_java);
 
+        setSupportActionBar(toolbar);
         String name = getIntent().getStringExtra("tt");
         getSupportActionBar().setTitle(name);
-        //ok
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, listItem);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value=adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
